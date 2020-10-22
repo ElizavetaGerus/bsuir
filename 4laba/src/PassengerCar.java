41503401e3f9b829c0ad2ed909b54570acc59e49
ElizavetaGerus <eizavetagerus@gmail.com>
2020-10-22 09:39:00 +0300

    lab 4

---
+++ 4laba/src/PassengerCar.java
@@ -0,0 +1,20 @@
+public abstract class PassengerCar implements Car, Object {
+    private float fuelConsumption;
+    private float accelerationTime;
+
+    public PassengerCar(){}
+
+    public PassengerCar(float fuelConsumption, float accelerationTime){
+        this.accelerationTime = accelerationTime;
+        this.fuelConsumption = fuelConsumption;
+    }
+
+    public float getFuelConsumption() {
+        return fuelConsumption;
+    }
+
+    public float getAccelerationTime() {
+        return accelerationTime;
+    }
+
+}
